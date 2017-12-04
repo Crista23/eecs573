ba09ba626dde69c0f81bb9c5cc086938f70f60c4
Georgeta-Cristina Garbacea <garbacea@gauss.si.umich.edu>
2017-12-04 15:32:06 -0500

    Plotting word2vec vectors

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -56,7 +56,7 @@ for doc in scan(es, query=query_text, index=INDEX):
 	#if  i == 10:
 	#	break
 
-model = Word2Vec(bug_details, min_count=1)
+model = Word2Vec(bug_details, min_count=200)
 
 X = model[model.vocab]
 pca = PCA(n_components=2)
@@ -65,7 +65,10 @@ result = pca.fit_transform(X)
 plt.scatter(result[:, 0], result[:, 1])
 words = list(model.vocab)
 for i, word in enumerate(words):
-	plt.annotate(word, xy=(result[i, 0], result[i, 1]))
+	plt.annotate(word, xy=(result[i, 0], result[i, 1]), size=8, ha='left', va='bottom')
+
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
 
 plt.tight_layout() #show plot with tight layout
 plt.savefig("word2vec_vectors.png") #show the plot
